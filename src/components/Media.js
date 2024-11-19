3deb69fe99e028d696a5082e82e45f7a023a3c72
sthk <soethihakyaw.mm.sthk@gmail>
2024-11-19 18:17:44 +0700

    [ADD] added media section

--- src/components/Media.js
+++ src/components/Media.js
@@ -6,10 +6,14 @@ export default function Media() {
     <section id="media" className="media">
       <h2>Media Gallery</h2>
       <div className="media-gallery">
-        <img src="/images/massage1.jpg" alt="Massage Therapy" />
-        <img src="/images/massage2.jpg" alt="Therapy Session" />
+        <img src="/assets/images/gallery/gallery1.jpg" alt="Massage Therapy" />
+        {/* <img src="/assets/images/gallery/gallery2.jpg" alt="Massage Therapy" /> */}
+        <img src="/assets/images/gallery/gallery3.jpg" alt="Massage Therapy" />
+        {/* <img src="/assets/images/gallery/gallery4.jpg" alt="Massage Therapy" /> */}
+        <img src="/assets/images/gallery/gallery5.jpg" alt="Massage Therapy" />
+        {/* <img src="/assets/images/gallery/gallery6.jpg" alt="Massage Therapy" /> */}
         <video controls>
-          <source src="/videos/massage-demo.mp4" type="video/mp4" />
+          <source src="/assets/videos/massage-demo.mp4" type="video/mp4" />
         </video>
       </div>
     </section>
